e9684035ab00ab5ac48474c5f844a8a04d2a037e
ManuelDLRS <up240399@alumnos.upa.edu.mx>
2026-02-28 10:54:16 -0600

    Tarea h5 Manuel De La Rosa Moreno terminada

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,61 @@
 module Helper exposing (..)
+
+
+grades : List Float
+grades =
+    [ 7, 9.8, 6.9, 0 ]
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade list =
+    List.map
+        (\grade ->
+            if grade >= 7 then
+                Approved
+
+            else if grade < 0 then
+                Pending
+
+            else
+                Failed
+        )
+        list
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction state =
+    case state of
+        OnTime ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+li : List AirplaneStatus
+li =
+    [ OnTime, Boarding, Cancelled, Delayed ]
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
